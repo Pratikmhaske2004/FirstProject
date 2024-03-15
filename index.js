91b6024a23e9ef27bc6164636e7287515f7162b5
Pratik Mhaske <127007422+Pratikmhaske2004@users.noreply.github.com>
2024-03-15 17:46:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = "Pratik";
+
+let username = prompt("Please enter a password");
+
+
+while(username != name){
+    usernum = prompt("Wrong! Try again:) \n please enter a correct password")
+
+
+}
+alert("welcome!! Sir");
